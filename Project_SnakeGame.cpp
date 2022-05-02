968cb2ff1b59ab7928b4fe5cede2132f94757dc9
Serendipity <95169197+Serendipityy@users.noreply.github.com>
2022-05-02 21:40:21 +0700

    Update Project_SnakeGame.cpp

--- Project_SnakeGame.cpp
+++ Project_SnakeGame.cpp
@@ -1,4 +1,4 @@
-﻿#include <windows.h> //for HWND
+#include <windows.h> //for HWND
 #include <iostream>
 #include <time.h> //for srand(time(NULL));
 #include <conio.h>//for getch()
@@ -11,11 +11,13 @@ namespace fs = std::experimental::filesystem;
 using namespace std;
 //constants
 #define MAX_SIZE_SNAKE 10
-#define MAX_SIZE_FOOD 4
+#define MAX_SIZE_FOOD 8
+#define MAX_SIZE_OBSTACLE 8
 #define MAX_SPEED 3
 //GLOBAL variables
 POINT snake[40];
-POINT food[4];
+POINT food[8];
+POINT obstacle[8];
 int MSSV[40] = { 2,1,1,2,0,4,4,9,
 2,1,1,2,0,4,5,8,
 2,1,1,2,0,4,6,4,
@@ -26,11 +28,13 @@ int MOVING;
 int SPEED;
 int HEIGH_CONSOLE, WIDTH_CONSOLE;
 int FOOD_INDEX;
+int OBSTACLE_INDEX;//chi so chuong ngai vat
 int SIZE_SNAKE;
 int STATE;
-int Score;
 int LEVEL;
+int Score;
 int threadrun = 1;
+
 // Cố định màn hình Console
 void FixconsoleWindow() {
 	HWND consoleWindow = GetConsoleWindow();
@@ -38,10 +42,8 @@ void FixconsoleWindow() {
 	style = style & ~(WS_MAXIMIZEBOX) & ~(WS_THICKFRAME);
 	SetWindowLong(consoleWindow, GWL_STYLE, style);
 }
-
 // Ẩn thanh cuộn trên màn hình Console
-void RemoveScrollbar()
-{
+void RemoveScrollbar() {
 	// get handle to the console window
 	HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
 
@@ -72,14 +74,8 @@ void RemoveScrollbar()
 
 	// print the current screen buffer dimensions
 	GetConsoleScreenBufferInfo(hOut, &scrBufferInfo);
-	cout << "Screen Buffer Size : " << scrBufferInfo.dwSize.X << " x "
-		<< scrBufferInfo.dwSize.Y << endl;
+	cout << "Screen Buffer Size : " << scrBufferInfo.dwSize.X << " x " << scrBufferInfo.dwSize.Y << endl;
 }
-
-void SetColor(int textColor, int backgroundColor) {
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), (textColor + (backgroundColor * 16)));
-}
-
 // Dịch chuyển
 void GotoXY(int x, int y) {
 	COORD coord;
@@ -94,21 +90,37 @@ bool IsValid(int x, int y) {
 			return false;
 	return true;
 }
-
+//Check if obstacle's position is food's position or not 
+bool IsValidObstacle(int x, int y, int width, int height) {
+	if (food[FOOD_INDEX].x >= x && food[FOOD_INDEX].x <= x + width)
+		return false;
+	return true;
+}
 void GenerateFood() {
 	int x, y;
 	srand(time(NULL));
 	for (int i = 0; i < MAX_SIZE_FOOD; i++) {
 		do {
-			x = rand() % (WIDTH_CONSOLE - 1) + 1;
+			x = 8 + rand() % (WIDTH_CONSOLE - 1) + 1; //I don't know why it's calculated like this
 			y = rand() % (HEIGH_CONSOLE - 1) + 1;
-		} while (IsValid(x, y) == false);
+		} while (IsValid(x, y) == false && IsValidObstacle(obstacle[OBSTACLE_INDEX].x, obstacle[OBSTACLE_INDEX].y, 4, 4) == false);
 		food[i] = { x,y };
 	}
 }
+void GenerateObstacle(int width, int height) {
+	int x, y;
+	srand(time(NULL));
+	for (int i = 0; i < MAX_SIZE_OBSTACLE; i++) {
+		do {
+			x = 8 + rand() % (WIDTH_CONSOLE - 1 - width) + 1;
+			y = rand() % (HEIGH_CONSOLE - 1 - height) + 1;
+		} while (IsValid(x, y) == false && IsValidObstacle(obstacle[OBSTACLE_INDEX].x, obstacle[OBSTACLE_INDEX].y, 4, 4) == false);
+		obstacle[i] = { x,y };
+	}
+}
 void ResetData() {
-	CHAR_LOCK = 'A', MOVING = 'D', SPEED = 1; FOOD_INDEX = 0, WIDTH_CONSOLE = 70,
-		HEIGH_CONSOLE = 20, SIZE_SNAKE = 6; Score = 0; LEVEL = 1;
+	CHAR_LOCK = 'A', MOVING = 'D', SPEED = 1; FOOD_INDEX = 0, OBSTACLE_INDEX = 0,
+		WIDTH_CONSOLE = 70, HEIGH_CONSOLE = 20, SIZE_SNAKE = 6; Score = 0, LEVEL = 1;
 	snake[0] = { 10, 5 }; snake[1] = { 11, 5 };
 	snake[2] = { 12, 5 }; snake[3] = { 13, 5 };
 	snake[4] = { 14, 5 }; snake[5] = { 15, 5 };
@@ -138,31 +150,7 @@ void ShowCur(bool CursorVisibility)
 	ConCurInf.bVisible = CursorVisibility;
 	SetConsoleCursorInfo(handle, &ConCurInf);
 }
-void DrawLine(int x, int y, int width) {
-	/*GotoXY(x + 1, y);
-	Color(9);
-	for (int i = 1; i < width; i++) cout << char(205);*/
-	GotoXY(x + 1, y);
-	Color(9);
-	for (int i = 1; i < width; i++) cout << char(219);
-}
 void DrawBoard(int x, int y, int width, int height, int curPosX = 0, int curPosY = 0) {
-	/*GotoXY(x, y);
-	Color(9);
-	char aa = char(219);
-	cout << char(201);
-	for (int i = 1; i < width; i++) cout << char(205);
-	cout << char(187);
-	GotoXY(x, height + y); cout << char(200);
-	for (int i = 1; i < width; i++) cout << char(205);
-	cout << char(188);
-	for (int i = y + 1; i < height + y; i++) {
-		GotoXY(x, i); cout << char(186);
-		GotoXY(x + width, i); cout << char(186);
-	}
-	Color(7);
-	GotoXY(curPosX, curPosY);*/
-
 	GotoXY(x, y);
 	Color(9);
 	for (int i = 0; i <= width; i++) cout << char(219);
@@ -175,22 +163,119 @@ void DrawBoard(int x, int y, int width, int height, int curPosX = 0, int curPosY
 	Color(7);
 	GotoXY(curPosX, curPosY);
 }
+void DrawTeam14() {
+	GotoXY(82, 1);
+	cout << "______";
+	GotoXY(82, 2);
+	cout << "|_   _|";
+	GotoXY(84, 3);
+	cout << "| |";
+	GotoXY(84, 4);
+	cout << "|_|";
+	GotoXY(89, 1);
+	cout << "_____";
+	GotoXY(89, 2);
+	cout << "| __|";
+	GotoXY(89, 3);
+	cout << "| `_";
+	GotoXY(89, 4);
+	cout << "|___,";
+	GotoXY(96, 1);
+	cout << "__";
+	GotoXY(96, 2);
+	cout << "/_\\";
+	GotoXY(95, 3);
+	cout << "/__ \\";
+	GotoXY(94, 4);
+	cout << "/_/ \\_\\";
+	GotoXY(101, 1);
+	cout << "___   ___";
+	GotoXY(101, 2);
+	cout << "|  \\_/  |";
+	GotoXY(101, 3);
+	cout << "| |\\_/| |";
+	GotoXY(101, 4);
+	cout << "|_|   |_|";
+	GotoXY(92, 6);
+	cout << "___";
+	GotoXY(92, 7);
+	cout << "| |";
+	GotoXY(92, 8);
+	cout << "| |";
+	GotoXY(92, 9);
+	cout << "|_|";
+	GotoXY(97, 6);
+	cout << "___";
+	GotoXY(97, 7);
+	cout << "/  |";
+	GotoXY(96, 8);
+	cout << "/_| |";
+	GotoXY(98, 9);
+	cout << "|_|";
+}
+void DrawSnakeGame() {
+	GotoXY(17, HEIGH_CONSOLE + 5);
+	cout << "__,";
+	GotoXY(17, HEIGH_CONSOLE + 6);
+	cout << "|_,";
+	GotoXY(17, HEIGH_CONSOLE + 7);
+	cout << "__/";
+	GotoXY(21, HEIGH_CONSOLE + 6);
+	cout << "||\\ |";
+	GotoXY(21, HEIGH_CONSOLE + 7);
+	cout << "|| \\|";
+	GotoXY(28, HEIGH_CONSOLE + 5);
+	cout << "__";
+	GotoXY(28, HEIGH_CONSOLE + 6);
+	cout << "/-\\";
+	GotoXY(27, HEIGH_CONSOLE + 7);
+	cout << "//`\\\\";
+	GotoXY(33, HEIGH_CONSOLE + 6);
+	cout << "||//";
+	GotoXY(33, HEIGH_CONSOLE + 7);
+	cout << "||\\\\";
+	GotoXY(38, HEIGH_CONSOLE + 5);
+	cout << "___,";
+	GotoXY(38, HEIGH_CONSOLE + 6);
+	cout << "|__";
+	GotoXY(38, HEIGH_CONSOLE + 7);
+	cout << "|__";
+	GotoXY(45, HEIGH_CONSOLE + 5);
+	cout << "____,";
+	GotoXY(45, HEIGH_CONSOLE + 6);
+	cout << "|| __";
+	GotoXY(45, HEIGH_CONSOLE + 7);
+	cout << "||_||";
+	GotoXY(52, HEIGH_CONSOLE + 5);
+	cout << "__";
+	GotoXY(52, HEIGH_CONSOLE + 6);
+	cout << "/-\\";
+	GotoXY(51, HEIGH_CONSOLE + 7);
+	cout << "//`\\\\";
+	GotoXY(57, HEIGH_CONSOLE + 6);
+	cout << "||\\/||";
+	GotoXY(57, HEIGH_CONSOLE + 7);
+	cout << "|| '||";
+	GotoXY(64, HEIGH_CONSOLE + 5);
+	cout << "___,";
+	GotoXY(64, HEIGH_CONSOLE + 6);
+	cout << "|__";
+	GotoXY(64, HEIGH_CONSOLE + 7);
+	cout << "|__";
+}
 void StartGame() {
 	system("cls");
 	ResetData();
 	fontsize(16, 16);
-	/*DrawBoard(75, 0, 30, HEIGH_CONSOLE);
-	DrawLine(75, 8, 30);
-	DrawLine(75, 16, 30);
-	DrawBoard(0, 0, WIDTH_CONSOLE, HEIGH_CONSOLE);*/
-
-	DrawBoard(70, 0, 29, HEIGH_CONSOLE);
-	DrawBoard(0, HEIGH_CONSOLE, WIDTH_CONSOLE + 29, 9);
-	DrawLine(70, 10, 29);
-	DrawBoard(0, 0, WIDTH_CONSOLE, HEIGH_CONSOLE);
-	GotoXY(77, 1);
+	DrawBoard(81, 0, 32, HEIGH_CONSOLE);
+	DrawBoard(8, HEIGH_CONSOLE + 4, WIDTH_CONSOLE, 5);
+	DrawBoard(81, HEIGH_CONSOLE + 2, 32, 7);
+	DrawBoard(8, 0, WIDTH_CONSOLE, HEIGH_CONSOLE);
+	DrawTeam14();
+	DrawSnakeGame();
+	GotoXY(87, HEIGH_CONSOLE - 5);
 	cout << "Score:" << Score;
-	GotoXY(77, HEIGH_CONSOLE - 3);
+	GotoXY(87, HEIGH_CONSOLE - 3);
 	cout << "LEVEL:" << LEVEL;
 	ShowCur(0);
 	STATE = 1;
@@ -204,22 +289,38 @@ void ExitGame(HANDLE t) {
 void PauseGame(HANDLE t) {
 	SuspendThread(t);
 }
-void Eat() {
-	//PlaySound(TEXT("eat sound.wav"), NULL, SND_ASYNC);
-	GotoXY(83, 1);
-	Score++;
-	cout << Score;
-	snake[SIZE_SNAKE] = food[FOOD_INDEX];
-	if (FOOD_INDEX == MAX_SIZE_FOOD - 1)
-	{
-		FOOD_INDEX = 0;
-		if (SPEED == MAX_SPEED) SPEED = 1;
-		else SPEED++;
-		GenerateFood();
+void DrawObstacle(int x, int y, int width, int height) {
+	GotoXY(x, y);
+	for (int i = 0; i < width; i++) cout << char(219);
+	GotoXY(x, height + y);
+	for (int i = 0; i < width; i++) cout << char(219);
+	for (int i = x; i < width + x; i++) {
+		for (int j = y + 1; j < height + y; j++) {
+			GotoXY(i, j); cout << char(219);
+		}
 	}
-	else {
-		FOOD_INDEX++;
-		SIZE_SNAKE++;
+}
+void DrawGate(int& n)
+{
+	int x, y;
+	srand((unsigned int)time(0));
+	do {
+		x = rand() % (WIDTH_CONSOLE - 1) + 1;
+		y = rand() % (HEIGH_CONSOLE - 1) + 1;
+	} while (IsValid(x, y) == false);
+	GotoXY(x, y);
+	putchar(176);
+}
+
+void ClearObstacle(int x, int y, int width, int height) {
+	GotoXY(x, y);
+	for (int i = 0; i < width; i++) printf("%c", ' ');
+	GotoXY(x, height + y);
+	for (int i = 0; i < width; i++) printf("%c", ' ');
+	for (int i = x; i < width + x; i++) {
+		for (int j = y + 1; j < height + y; j++) {
+			GotoXY(i, j); printf("%c", ' ');
+		}
 	}
 }
 void ProcessDead() {
@@ -227,6 +328,44 @@ void ProcessDead() {
 	//PlaySound(TEXT("oh no sound.wav"), NULL, SND_ASYNC);
 	GotoXY(0, HEIGH_CONSOLE + 2);
 	printf("Dead, type y to continue or anykey to exit");
+}
+void Eat() {
+	//PlaySound(TEXT("eat sound.wav"), NULL, SND_ASYNC);
+	GotoXY(93, HEIGH_CONSOLE - 5);
+	Score++;
+	cout << Score;
+	snake[SIZE_SNAKE] = food[FOOD_INDEX];
+	if (FOOD_INDEX == MAX_SIZE_FOOD - 1)
+	{
+		FOOD_INDEX = 0;
+		GotoXY(83, HEIGH_CONSOLE - 3);
+		LEVEL++;
+		cout << LEVEL;
+		if (SPEED == MAX_SPEED) SPEED = 1;
+		else SPEED++;
+		GenerateFood();
+	}
+	else {
+		if (LEVEL == 1) { //Tạo chướng ngại vật màn 1
+			ClearObstacle(obstacle[OBSTACLE_INDEX - 1].x, obstacle[OBSTACLE_INDEX - 1].y, 4, 4);
+			GenerateObstacle(4, 4);
+			DrawObstacle(obstacle[OBSTACLE_INDEX].x, obstacle[OBSTACLE_INDEX].y, 4, 4);
+			if (OBSTACLE_INDEX == MAX_SIZE_OBSTACLE - 1)
+				OBSTACLE_INDEX = 0;
+			OBSTACLE_INDEX++;
+		}
+		FOOD_INDEX++;
+		SIZE_SNAKE++;
+	}
+}
+void setFontSize(int FontSize)
+{
+	CONSOLE_FONT_INFOEX info = { 0 };
+	info.cbSize = sizeof(info);
+	info.dwFontSize.Y = FontSize; // leave X as zero
+	info.FontWeight = FW_NORMAL;
+	wcscpy_s(info.FaceName, L"Lucida Console");
+	SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), NULL, &info);
 }
 void ClearSnakeAndFood(char str) {
 	GotoXY(food[FOOD_INDEX].x, food[FOOD_INDEX].y);
@@ -256,8 +395,18 @@ bool SnakeTouchBody(int x, int y)
 	}
 	return false;
 }
+bool SnakeTouchObstacle(int x, int y, int width, int height)
+{
+	if (x >= obstacle[OBSTACLE_INDEX - 1].x && x <= obstacle[OBSTACLE_INDEX - 1].x + width
+		&& y >= obstacle[OBSTACLE_INDEX - 1].y && y <= obstacle[OBSTACLE_INDEX - 1].y + height)
+	{
+		return true;
+	}
+	return false;
+}
 void MoveRight() {
-	if (snake[SIZE_SNAKE - 1].x + 1 == WIDTH_CONSOLE || (SnakeTouchBody(snake[SIZE_SNAKE - 1].x + 1, snake[SIZE_SNAKE - 1].y) == true)) {
+	if (snake[SIZE_SNAKE - 1].x + 1 == WIDTH_CONSOLE + 8 || (SnakeTouchBody(snake[SIZE_SNAKE - 1].x + 1, snake[SIZE_SNAKE - 1].y) == true)
+		|| SnakeTouchObstacle(snake[SIZE_SNAKE - 1].x + 1, snake[SIZE_SNAKE - 1].y, 4, 4) == true) {
 		ProcessDead();
 	}
 	else {
@@ -270,8 +419,10 @@ void MoveRight() {
 		}
 		snake[SIZE_SNAKE - 1].x++;
 	}
-}void MoveLeft() {
-	if (snake[SIZE_SNAKE - 1].x - 1 == 0 || (SnakeTouchBody(snake[SIZE_SNAKE - 1].x - 1, snake[SIZE_SNAKE - 1].y) == true)) {
+}
+void MoveLeft() {
+	if (snake[SIZE_SNAKE - 1].x - 1 == 8 || (SnakeTouchBody(snake[SIZE_SNAKE - 1].x - 1, snake[SIZE_SNAKE - 1].y) == true)
+		|| SnakeTouchObstacle(snake[SIZE_SNAKE - 1].x + 1, snake[SIZE_SNAKE - 1].y, 4, 4) == true) {
 		ProcessDead();
 	}
 	else {
@@ -286,7 +437,8 @@ void MoveRight() {
 	}
 }
 void MoveDown() {
-	if (snake[SIZE_SNAKE - 1].y + 1 == HEIGH_CONSOLE || (SnakeTouchBody(snake[SIZE_SNAKE - 1].x, snake[SIZE_SNAKE - 1].y + 1) == true)) {
+	if (snake[SIZE_SNAKE - 1].y + 1 == HEIGH_CONSOLE || (SnakeTouchBody(snake[SIZE_SNAKE - 1].x, snake[SIZE_SNAKE - 1].y + 1) == true)
+		|| SnakeTouchObstacle(snake[SIZE_SNAKE - 1].x + 1, snake[SIZE_SNAKE - 1].y, 4, 4) == true) {
 		ProcessDead();
 	}
 	else {
@@ -301,7 +453,8 @@ void MoveDown() {
 	}
 }
 void MoveUp() {
-	if (snake[SIZE_SNAKE - 1].y - 1 == 0 || (SnakeTouchBody(snake[SIZE_SNAKE - 1].x, snake[SIZE_SNAKE - 1].y - 1) == true)) {
+	if (snake[SIZE_SNAKE - 1].y - 1 == 0 || (SnakeTouchBody(snake[SIZE_SNAKE - 1].x, snake[SIZE_SNAKE - 1].y - 1) == true)
+		|| SnakeTouchObstacle(snake[SIZE_SNAKE - 1].x + 1, snake[SIZE_SNAKE - 1].y, 4, 4) == true) {
 		ProcessDead();
 	}
 	else {
@@ -382,7 +535,6 @@ void ProcessSave() {
 	SaveData(name);
 }
 void main() {
-	//SetColor(0, 66);
 	int temp;
 	FixconsoleWindow();
 	RemoveScrollbar();
