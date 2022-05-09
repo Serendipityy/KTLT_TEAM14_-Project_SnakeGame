83d36fec29c5414a0b488308a005302a174843c5
common-maker <69307908+common-maker@users.noreply.github.com>
2022-05-09 20:31:08 +0700

    Add files via upload

---
+++ nkea.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+#include <windows.h> //for HWND
+#include <iostream>
+#include <time.h> //for srand(time(NULL));
+#include <conio.h>//for getch()
+#include <thread>
+#include "dirent.h"
+#include <vector>
+#include <iomanip>
+#include <mutex>
+#include <string>
+#include <fstream>//for read file
+using namespace std;
+#pragma warning(disable: 4996)
+//Cố định màn hình Console
+void FixconsoleWindow();
+// Chỉnh màu chữ 
+void setTextColor(int color);
+// Chỉnh màu background
+void BackGroundColor(WORD color);
+// Dịch chuyển
+void GotoXY(int x, int y);
+// Check if obstacle's position is food's position or not
+//bool IsValidObstacle(int x, int y, int width, int height);
+// change fontsize - CAN HAY KHONG
+void fontsize(int a, int b);
+// xoa con tro neu ShowCur(0);
+void ShowCur(bool CursorVisibility);
+void DrawBoard(int x, int y, int width, int height, int curPosX = 0, int curPosY = 0);
+//drawboard cho menu
+void Draw_Board(int x, int y, int width, int height, int curPosX = 0, int curPosY = 0);
+
+void DrawSnakeGame_Menu(char Text, int x, int y);
+void DrawSnake_Game();
+void DrawSnake_Text();
+void DrawTeam14();
+void DrawGameOver();
+void DrawHowToPlay();
+//tinh toan vi tri in menu_list
+int pos_calc(string s);
+//ve intro con ran ngau vcl
+void Draw_Newgame_intro();
+//ve bang menu save game load setiing..
+void Draw_menu_board();
