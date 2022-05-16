2e18fd0fc5119aeb984c1fa2b2a0054adeb6a8d0
Huyen Nguyen <93507098+NgHuyen137@users.noreply.github.com>
2022-05-17 00:59:11 +0700

    Update nkea.h

--- snake_game/snake_game/nkea.h
+++ snake_game/snake_game/nkea.h
@@ -1,56 +1,90 @@
 ﻿#pragma once
-//#include <mmsystem.h>
-#include <windows.h> //for HWND
 #pragma comment(lib, "Winmm.lib")
+#pragma warning(disable: 4996)
+#include <windows.h> //for HWND
 #include <iostream>
 #include <time.h> //for srand(time(NULL));
-#include <conio.h>//for getch()
-#include <thread>
+#include <conio.h> //for getch()
+#include <thread> //for thread
 #include "dirent.h"
 #include <vector>
 #include <iomanip>
 #include <mutex>
 #include <string>
-#include <fstream>//for read file
+#include <fstream> //for read file
 using namespace std;
-#pragma warning(disable: 4996)
+
 //Cố định màn hình Console
 void FixconsoleWindow();
+
 // Chỉnh màu chữ 
 void setTextColor(int color);
-// Chỉnh màu background
-void BackGroundColor(WORD color);
+
 // Dịch chuyển
 void GotoXY(int x, int y);
-// Check if obstacle's position is food's position or not
-//bool IsValidObstacle(int x, int y, int width, int height);
-// change fontsize - CAN HAY KHONG
+
+// Thay đổi font chữ
 void fontsize(int a, int b);
-// xoa con tro neu ShowCur(0);
+
+// Xóa con trỏ nếu ShowCur(0);
 void ShowCur(bool CursorVisibility);
+
+//Vẽ khung game
 void DrawBoard(int x, int y, int width, int height, int curPosX = 0, int curPosY = 0);
-//drawboard cho menu
+
+//Drawboard cho menu
 void Draw_Board(int x, int y, int width, int height, int curPosX = 0, int curPosY = 0);
 
+//Vẽ chữ SNAKE GAME trong menu
 void DrawSnakeGame_Menu(char Text, int x, int y);
+
+//Vẽ con rắn 
 void DrawSnake_Game();
+
+//Vẽ chứ SNAKE
 void DrawSnake_Text();
+
+//Vẽ chữ TEAM 14
 void DrawTeam14();
+
+//Vẽ chữ GAME OVER
 void DrawGameOver();
+
+//Vẽ hướng dẫn chơi
 void DrawHowToPlay();
-//tinh toan vi tri in menu_list
+
+//Tính toán vị trí in menu_list
 int pos_calc(string s);
-//ve intro con ran ngau vcl
+
+//Vẽ intro con rắn
 void Draw_Newgame_intro();
-//ve bang menu save game load setiing..
+
+//Vẽ bảng menu save game load setiing..
 void Draw_menu_board();
+
 //changelog
 void ThreadAnimation();
+
+//Vẽ màn hình chờ game over
 void draw_gameover();
+
+//Vẽ nhện
 bool DrawSpider();
+
+//Khởi chạy rắn
 void Run();
+
+//Vẽ màn chơi
 void DrawMapLv(int level_index);
+
+//Vẽ màn hình chơi và khởi tạo dữ liệu ban đầu cho game
 void Start();
+
+//Vẽ rắn và thức ăn
 void DrawSnakeAndFood(char str);
+
+//Xóa rắn và thức ăn
 void ClearSnakeAndFood(char str);
+
+//Kết thúc game
 void Draw_endgame();
